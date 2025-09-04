3684d134c29277aa0b8f486b2c44bb5e54562399
lamyea <tsmim08@gmail.com>
2025-09-04 18:29:52 +0600

    Changes

--- Mid Lab Task/WEB/VIEW/script.js
+++
@@ -1,7 +0,0 @@
-function editItem(id, name, category, price) {
-    document.getElementById('editForm').style.display = 'block';
-    document.getElementById('edit_id').value = id;
-    document.getElementById('edit_name').value = name;
-    document.getElementById('edit_category').value = category;
-    document.getElementById('edit_price').value = price;
-}
